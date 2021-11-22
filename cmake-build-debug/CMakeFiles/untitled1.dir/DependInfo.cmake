35d90e3a9df7f19c25741a0ffaac1e9be38e5c7c
prg-original <igormonardez@id.uff.br>
2021-11-22 11:47:57 -0300

    Por prg-original: Função de clonar o arquivo para um auxiliar que iremos usar no desenvolvimento do pré processador

--- cmake-build-debug/CMakeFiles/untitled1.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/untitled1.dir/DependInfo.cmake
@@ -8,7 +8,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_C
-  "C:/Users/Profissional/CLionProjects/Pre-processador/main.c" "C:/Users/Profissional/CLionProjects/Pre-processador/cmake-build-debug/CMakeFiles/untitled1.dir/main.c.obj"
+  "C:/Users/Igor/CLionProjects/Pre-processador/main.c" "C:/Users/Igor/CLionProjects/Pre-processador/cmake-build-debug/CMakeFiles/untitled1.dir/main.c.obj"
   )
 set(CMAKE_C_COMPILER_ID "GNU")
 
--- main.c
+++ main.c
@@ -1,8 +1,78 @@
 #include <stdio.h>
+#include "stdlib.h"
+#include <ctype.h>
+#include "locale.h"
+
+
+void clone(FILE *arq_original, FILE *arq_destino);
+
+void str_trim(char *trim_origem,char *trim_destino);
 
 int main(){
-    int x;
-    scanf("%d", &x);
-    printf("Ola Fernando!!!%d\n", x);
+    char conteudo, auxiliar[100], arq_original[100],arq_destino[100];
+    FILE *origem,*destino;
+
+    printf("Arquivo de origem\n> ");
+    fgets(auxiliar,100,stdin);
+    str_trim(auxiliar,arq_original);
+    origem = fopen(arq_original,"r");
+
+    if(origem == NULL)
+        printf("Não existe arquivo de origem\n");
+    else{
+        printf("Arquivo de destino:\n> ");
+        fgets(arq_destino,100,stdin);
+        destino = fopen(arq_destino,"w");
+        clone(origem,destino);
+    }
+
+    fclose(origem);
+    fclose(destino);
     return 0;
+}
+
+void clone(FILE *origem, FILE *destino){
+    char conteudo;
+    int linhas = 0, linha_atual = 0;
+    while((conteudo = fgetc(origem))!=EOF)
+        linhas++;
+
+    rewind(origem);
+
+    while((conteudo = fgetc(origem))!= EOF){
+        fputc(conteudo, destino);
+    }
+    printf("\nArquivo copiado com sucesso\n\n");
+
+}
+
+void str_trim(char *trim_origem, char *trim_destino) {
+    int flag = 0;
+
+
+    while(*trim_origem) {
+        if(!isspace((unsigned char) *trim_origem) && flag == 0) {
+            *trim_destino++ = *trim_origem;
+            flag = 1;
+        }
+
+
+        trim_origem++;
+        if(flag == 1) {
+            *trim_destino++ = *trim_origem;
+        }
+    }
+
+
+    while(1) {
+        trim_destino--;
+
+
+        if(!isspace((unsigned char) *trim_destino) && flag == 0) {
+            break;
+        }
+
+        flag = 0;
+        *trim_destino = '\0';
+    }
 }
