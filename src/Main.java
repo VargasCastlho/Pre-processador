3608633e6509ee5303c16afa5fa2fef5804f4dfd
Fernando Vargas <fecastilhovargas@id.uff.br>
2021-12-01 21:34:19 -0300

    Por fcvargas: refatorado método para tratar arquivo.

--- src/Main.java
+++ src/Main.java
@@ -1,88 +1,37 @@
+import javax.swing.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         String nome;
-        //nome = JOptionPane.showInputDialog(null, "Entre com o nome do arquivo:");
-        nome = "arquivo_entrada.c";
-        AtivarCriarArquivoProcessado(nome);
-        AtivarTratarBibliotecaExterna(nome);
-        AtivarComentarioSimples(nome);
-        AtivarComentarioComplexo(nome);
-        AtivarTratarDefine(nome);
-        AtivarCompactacao(nome);
-        AtivarTratarBiblioteca(nome);
-        AtivarComentarioSimples(nome);
-        AtivarComentarioComplexo(nome);
-        AtivarTratarLinha(nome);
+        nome = JOptionPane.showInputDialog(null, "Entre com o nome do arquivo:");
+        String arquivoFinal = AtivarCriarArquivoProcessado(nome);
+        arquivoFinal = ProcessamentoArquivo.TratarBibliotecaExterna(arquivoFinal);
+        arquivoFinal = ProcessamentoArquivo.ComentarioSimples(arquivoFinal);
+        arquivoFinal = ProcessamentoArquivo.ComentarioComplexo(arquivoFinal);
+        arquivoFinal = ProcessamentoArquivo.TratarDefine(arquivoFinal);
+        arquivoFinal = ProcessamentoArquivo.tratarCompactacao(arquivoFinal);
+        arquivoFinal = ProcessamentoArquivo.TratarBiblioteca(arquivoFinal);
+        arquivoFinal = ProcessamentoArquivo.ComentarioSimples(arquivoFinal);
+        arquivoFinal = ProcessamentoArquivo.ComentarioComplexo(arquivoFinal);
+        arquivoFinal = ProcessamentoArquivo.TratarLinha(arquivoFinal);
+        EscreverArquivo(arquivoFinal, nome);
     }
 
-    static void AtivarCriarArquivoProcessado(String nome) throws IOException {
+    static String AtivarCriarArquivoProcessado(String nome) throws IOException {
         BufferedReader br;
         String arquivo;
         br = TratamentoArquivo.LeituraAbrirArquivo(nome);
         arquivo = ProcessamentoArquivo.CriarArquivoProcessado(br);
         TratamentoArquivo.LeituraFecharArquivo(br);
-        TratamentoArquivo.EscritaArquivo(arquivo, nome);
+        return arquivo;
     }
 
-    static void AtivarComentarioSimples(String nome) throws IOException {
+    static void EscreverArquivo(String arquivoFinal, String nome) throws IOException {
         BufferedReader br;
-        String arquivo;
         br = TratamentoArquivo.LeituraAbrirArquivo(TratamentoArquivo.NomeProcessado(nome));
-        arquivo = ProcessamentoArquivo.ComentarioSimples(br);
         TratamentoArquivo.LeituraFecharArquivo(br);
-        TratamentoArquivo.EscritaArquivo(arquivo, nome);
-    }
-
-    static void AtivarComentarioComplexo(String nome) throws IOException {
-        BufferedReader br;
-        String arquivo;
-        br = TratamentoArquivo.LeituraAbrirArquivo(TratamentoArquivo.NomeProcessado(nome));
-        arquivo = ProcessamentoArquivo.ComentarioComplexo(br);
-        TratamentoArquivo.LeituraFecharArquivo(br);
-        TratamentoArquivo.EscritaArquivo(arquivo, nome);
-    }
-
-    static void AtivarTratarBiblioteca(String nome) throws IOException {
-        BufferedReader br;
-        String arquivo;
-        br = TratamentoArquivo.LeituraAbrirArquivo(TratamentoArquivo.NomeProcessado(nome));
-        arquivo = ProcessamentoArquivo.TratarBiblioteca(br);
-        TratamentoArquivo.LeituraFecharArquivo(br);
-        TratamentoArquivo.EscritaArquivo(arquivo, nome);
-    }
-
-    static void AtivarTratarDefine(String nome) throws IOException {
-        BufferedReader br;
-        br = TratamentoArquivo.LeituraAbrirArquivo(TratamentoArquivo.NomeProcessado(nome));
-        ProcessamentoArquivo.TratarDefine(br, nome);
-    }
-
-    static void AtivarCompactacao(String nome) throws IOException {
-        BufferedReader br;
-        br = TratamentoArquivo.LeituraAbrirArquivo(TratamentoArquivo.NomeProcessado(nome));
-        ProcessamentoArquivo.tratarCompactacao(br, nome);
-        br = TratamentoArquivo.LeituraAbrirArquivo(TratamentoArquivo.NomeProcessado(nome));
-        ProcessamentoArquivo.compactacaoTotal(br, nome);
-    }
-
-    static void AtivarTratarLinha(String nome) throws IOException {
-        BufferedReader br;
-        String arquivo;
-        br = TratamentoArquivo.LeituraAbrirArquivo(TratamentoArquivo.NomeProcessado(nome));
-        arquivo = ProcessamentoArquivo.TratarLinha(br);
-        TratamentoArquivo.LeituraFecharArquivo(br);
-        TratamentoArquivo.EscritaArquivo(arquivo, nome);
-    }
-
-    static void AtivarTratarBibliotecaExterna(String nome) throws IOException {
-        BufferedReader br;
-        String arquivo;
-        br = TratamentoArquivo.LeituraAbrirArquivo(TratamentoArquivo.NomeProcessado(nome));
-        arquivo = ProcessamentoArquivo.TratarBibliotecaExterna(br);
-        TratamentoArquivo.LeituraFecharArquivo(br);
-        TratamentoArquivo.EscritaArquivo(arquivo, nome);
+        TratamentoArquivo.EscritaArquivo(arquivoFinal, nome);
     }
 }
--- src/ProcessamentoArquivo.java
+++ src/ProcessamentoArquivo.java
@@ -6,11 +6,11 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ProcessamentoArquivo {
-    static String ComentarioSimples(BufferedReader br) throws IOException {
+    static String ComentarioSimples(String arquivoFinal) throws IOException {
         String arq = "";
         String linha;
-        while (br.ready()) {
-            linha = br.readLine() + "\n";
+        for(int i = 0; i<arquivoFinal.split("\n").length;i++){
+            linha = arquivoFinal.split("\n")[i] + "\n";
             if (!linha.contains("//"))
                 arq += linha;
             else {
@@ -24,11 +24,11 @@ public class ProcessamentoArquivo {
         return arq;
     }
 
-    static String ComentarioComplexo(BufferedReader br) throws IOException {
+    static String ComentarioComplexo(String arquivoFinal) throws IOException {
         String arq = "";
         String linha;
-        while (br.ready()) {
-            linha = br.readLine() + "\n";
+        for(int i = 0; i<arquivoFinal.split("\n").length;i++){
+            linha = arquivoFinal.split("\n")[i] + "\n";
             if (!linha.contains("/*"))
                 arq += linha;
             else {
@@ -40,7 +40,7 @@ public class ProcessamentoArquivo {
                 while (!linha.contains("*/")) {
                     linha = "";//tirar todas as linhas enquanto não encontrar o "*/"
                     arq += linha;
-                    linha = br.readLine();
+                    linha = arquivoFinal.split("\n")[++i];
                 }
                 //chegando aqui quer dizer que a linha contem o "*/"
                 int indexSegundo = linha.indexOf("*/");
@@ -54,15 +54,15 @@ public class ProcessamentoArquivo {
         return arq;
     }
 
-    static String TratarBiblioteca(BufferedReader br) {
+    static String TratarBiblioteca(String arquivoFinal) {
         String arq = "";
         String arquivo = "";
         String biblioteca;
         String linha = "";
         BufferedReader aux;
         try {
-            while (br.ready()) {
-                linha = br.readLine() + "\n";
+            for(int i = 0; i<arquivoFinal.split("\n").length;i++){
+                linha = arquivoFinal.split("\n")[i] + "\n";
                 if (linha.contains("#include")&&linha.substring(linha.indexOf("#include ") + 9).startsWith("<")) {
                     String include = "#include ";
                     int index = linha.indexOf(include) + 10;
@@ -91,13 +91,13 @@ public class ProcessamentoArquivo {
         return arq;
     }
 
-    static void TratarDefine(BufferedReader br, String nome) throws IOException {
+    static String TratarDefine(String arquivoFinal) throws IOException {
         Map<String, String> variavelValor = new HashMap<>();
         ArrayList<String> variavelNome = new ArrayList();
         String linha, linhaDef;
         String nomeVariavel, valorVariavel;
-        while (br.ready()) {
-            linha = br.readLine() + "\n";
+        for(int i = 0; i<arquivoFinal.split("\n").length;i++){
+            linha = arquivoFinal.split("\n")[i] + "\n";
             if (linha.contains("#define")) {
                 int indexNomeVariavel = linha.indexOf("#define") + 8;
                 linhaDef = linha.substring(indexNomeVariavel, linha.length() - 1);
@@ -108,20 +108,17 @@ public class ProcessamentoArquivo {
                 variavelNome.add(nomeVariavel);
             }
         }
-        TratamentoArquivo.LeituraFecharArquivo(br);
-        DefinirTratarDefine(variavelValor, variavelNome, nome);
+        return DefinirTratarDefine(variavelValor, variavelNome, arquivoFinal);
     }
 
-    static void DefinirTratarDefine(Map variavelValor, ArrayList<String> variavelNome, String nome) throws IOException {
-        BufferedReader br;
+    static String DefinirTratarDefine(Map variavelValor, ArrayList<String> variavelNome, String arquivoFinal) throws IOException {
         String linha = "", linhaAntes, linhaDps;
         String arq = "";
         for (int j = 0; j < variavelNome.size(); j++) {
             for (int i = 0; i < variavelNome.size(); i++) {
-                br = TratamentoArquivo.LeituraAbrirArquivo(TratamentoArquivo.NomeProcessado(nome));
                 arq = "";
-                while (br.ready()) {
-                    linha = br.readLine() + "\n";
+                for(int x = 0; x<arquivoFinal.split("\n").length;x++){
+                    linha = arquivoFinal.split("\n")[x] + "\n";
                     if (linha.contains(variavelNome.get(i))) {
                         if (linha.substring(0, linha.indexOf(variavelNome.get(i))).equals("#define ") && linha.substring(0, linha.indexOf(variavelNome.get(i)) + variavelNome.get(i).length()).equals("#define " + variavelNome.get(i))) {
                             linha = "";
@@ -133,98 +130,94 @@ public class ProcessamentoArquivo {
                     }
                     arq += linha;
                 }
-                TratamentoArquivo.LeituraFecharArquivo(br);
-                TratamentoArquivo.EscritaArquivo(arq, nome);
+                arquivoFinal = arq;
             }
         }
+        return arq;
     }
 
-    static void tratarCompactacao(BufferedReader br,String nome) throws IOException {
+    static String tratarCompactacao(String arquivoFinal) throws IOException {
         //1ºpasso compactar toda a linha particularmente com as restrições quanto à função
         String linha;
         String arq = "";
-        while(br.ready()) {
+        for(int x = 0; x<arquivoFinal.split("\n").length;x++){
             int verifica = 0;
-            linha=br.readLine()+"\n";
+            linha = arquivoFinal.split("\n")[x] + "\n";
             ArrayList<String> vetor = new ArrayList();
             vetor.add("int");vetor.add("char");vetor.add("return");vetor.add("void");vetor.add("typedef struct");
             for(int i = 0; i<vetor.size(); i++) {
                 if (linha.contains(vetor.get(i))) {
+
                     String string = vetor.get(i);
                     linha = linha.replace(string + " ", string + "´");//tombei apenas os espaços pós tipos ou returns para diferenciar de outro espaço
-                    linha = linha.replace(" ", "");
+                    if(linha.contains("\"")) {
+                        String linhaAntes, linhaDps;
+                        linhaAntes = linha.substring(0, linha.indexOf("\""));
+                        linhaDps = linha.substring(linha.lastIndexOf("\"") + 1);
+                        linhaAntes = linhaAntes.replace(" ", "");
+                        linhaDps = linhaDps.replace(" ", "");
+                        linha = linhaAntes + linha.substring(linha.indexOf("\""), linha.lastIndexOf("\"") + 1) + linhaDps;
+                    }
+                    else
+                        linha = linha.replace(" ", "");
                     linha = linha.replace(string + "´", string + " ");
                     verifica = 1;
                 }
             }
             if(verifica == 0){
-
-                // Após tombar os espaçoes que não queremos retirar retiraremos todos os outros espaços que não queremos retirar.
-
-                linha = linha.replace(" ", "");
-
-
-                //Depois de retirar os outros espaços que queremos tirar, retornaremos com os espaços tombados posteriormentes
+                    linha = linha.replace(" ", "");
             }
             arq += linha;
 
         }
-        TratamentoArquivo.LeituraFecharArquivo(br);
-        TratamentoArquivo.EscritaArquivo(arq, nome);
+        return compactacaoTotal(arq);
     }
 
-    static void compactacaoTotal(BufferedReader br,String nome) throws IOException {
+    static String compactacaoTotal(String arquivoFinal) throws IOException {
         //2º juntar todas as linhas brutalmente, já que só falta isso.
         String linha;
         String arq = "";
-        while(br.ready()) {
-            linha = br.readLine();
+        for(int i = 0; i<arquivoFinal.split("\n").length;i++){
+            linha = arquivoFinal.split("\n")[i];
             if(linha.contains("#include")||linha.contains("#define"))
                 arq += linha + "\n";
             else
                 arq += linha;
         }
-        TratamentoArquivo.LeituraFecharArquivo(br);
-        TratamentoArquivo.EscritaArquivo(arq, nome);
+        return arq;
     }
 
-    static String TratarLinha(BufferedReader br) {
+    static String TratarLinha(String arquivoFinal) {
         String arq = "";
         String linha = "";
-        try {
-            while (br.ready()) {
-                linha = br.readLine() + "\n";
+            for(int i = 0; i<arquivoFinal.split("\n").length;i++){
+                linha = arquivoFinal.split("\n")[i] + "\n";
                 if(linha.isBlank()){
                     linha = "";
                 }
                 arq += linha;
             }
-        } catch (FileNotFoundException e) {
-            System.out.println("Erro: " + e.getMessage());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
         return arq;
     }
 
-    static String TratarBibliotecaExterna(BufferedReader br) {
+    static String TratarBibliotecaExterna(String arquivoFinal) {
         String arq = "";
         String arquivo = "";
         String biblioteca;
         String linha = "";
         BufferedReader aux;
         try {
-            while (br.ready()) {
-                linha = br.readLine() + "\n";
+            for(int i = 0; i<arquivoFinal.split("\n").length;i++){
+                linha = arquivoFinal.split("\n")[i] + "\n";
                 if (linha.contains("#include")&&linha.substring(linha.indexOf("#include ") + 9).startsWith("\"")) {
                     String include = "#include ";
                     int index = linha.indexOf(include) + 10;
                     biblioteca = linha.substring(index, linha.length() - 2);
-                    System.out.println(biblioteca);
                     aux = TratamentoArquivo.LeituraAbrirArquivo("C:\\MinGW\\include\\" + biblioteca);
                     while (aux.ready()) {
                         arquivo += aux.readLine() + "\n";
                     }
+                    TratamentoArquivo.LeituraFecharArquivo(aux);
                     linha = arquivo;
                 }
                 arq += linha;
