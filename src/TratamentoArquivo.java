99f44f249c2238ba9f451041192332a5e802ecd6
Fernando Vargas <fecastilhovargas@id.uff.br>
2021-12-06 10:49:59 -0300

    por fcvargas: refatoração.

--- src/TratamentoArquivo.java
+++ src/TratamentoArquivo.java
@@ -1,8 +1,11 @@
 import java.io.*;
 
 public class TratamentoArquivo {
-    static String NomeProcessado(String nome) {
-        return nome.substring(0, nome.length() - 2) + "_pre_processado.c";
+    static String NomeProcessado(String nome) throws IOException {
+        String path = nome.substring(0, nome.length() - 2) + "_pre_processado.c";
+        File f = new File(path);
+        f.createNewFile();
+        return path;
     }
 
     static BufferedReader LeituraAbrirArquivo(String nome) throws IOException {
